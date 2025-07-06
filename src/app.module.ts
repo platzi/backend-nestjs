983a7687c3b48091912fd0fe410fa4c43cc1b665
Nicolas Molina Monroy <hi@nicobytes.com>
2025-07-06 14:53:42 -0400

    Class 9: Refactor: Remove AppController and AppService, introduce UsersModule
    - Deleted AppController and AppService as part of the refactor.
    - Introduced UsersModule to encapsulate user-related functionality.
    - Updated app.module.ts to include UsersModule, streamlining the application structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
   ],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
 })
 export class AppModule {}
